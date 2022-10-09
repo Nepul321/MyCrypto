24d14e882ad7a7a024e71b863ca2c991c56ada80
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-10-08 22:41:29 -0400

    12 - Create a basic portfolio home page

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from base.models import Portfolio
 
 def HomeView(request, *args, **kwargs):
+    user = request.user
+    obj = Portfolio.objects.get(user=user)
     template = "home.html"
     context = {
- 
+       'obj' : obj
     }
 
     return render(request, template, context)

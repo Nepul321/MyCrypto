4dc9de65ae227fdec86eff45324cd61a0367ca70
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-10-10 14:20:45 -0400

    14 - Start experimenting with the API

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,21 @@
-// JS
+const cryptosinportfolio = ['Bitcoin', 'Ethereum']
+
+function getCryptos() {
+    fetch('https://api.coinlore.net/api/tickers/')
+
+    .then((res) => {
+        return res.json();
+    })
+    
+    .then((data) => {
+        items = data.data
+        useData(items)
+    })
+}
+
+function useData(data) {
+  const items = data.filter(item => cryptosinportfolio.includes(item.name))
+  console.log(items)
+}
+
+getCryptos();

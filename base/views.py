6eb4c138a13c12b568d47e45e5b85f5b3f9fa1be
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-10-11 17:13:11 -0400

    17 - Update HomeView

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from base.models import Portfolio
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def HomeView(request, *args, **kwargs):
     user = request.user
     obj = Portfolio.objects.get(user=user)
